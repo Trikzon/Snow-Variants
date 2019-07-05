978b354a731e7aff2d74dce66612a10ec8639d4d
Trikzon <trikzongaming@gmail.com>
2019-07-05 09:07:33 -0400

    SnowSlab now has slab shape

--- src/main/java/trikzon/snowvariants/blocks/SnowSlab.java
+++ src/main/java/trikzon/snowvariants/blocks/SnowSlab.java
@@ -1,7 +1,12 @@
 package trikzon.snowvariants.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
 import trikzon.snowvariants.SnowVariants;
 
 public class SnowSlab extends Block {
@@ -20,4 +25,11 @@ public class SnowSlab extends Block {
     public BlockRenderLayer getRenderLayer() {
         return BlockRenderLayer.CUTOUT_MIPPED;
     }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D);
+    }
+
+
 }
