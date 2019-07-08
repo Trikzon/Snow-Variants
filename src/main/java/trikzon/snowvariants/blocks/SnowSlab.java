89e95f81e7fd977f46c352fdf15dfe34ab61d5a2
Trikzon <trikzongaming@gmail.com>
2019-07-08 04:36:15 -0400

    Fixed Raytracing - 1.14.3-1.0.1

--- src/main/java/trikzon/snowvariants/blocks/SnowSlab.java
+++ src/main/java/trikzon/snowvariants/blocks/SnowSlab.java
@@ -10,10 +10,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShovelItem;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.state.properties.SlabType;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
@@ -47,6 +44,8 @@ public class SnowSlab extends Block {
     @Override
     public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
         if (player.getHeldItem(handIn).getItem() instanceof ShovelItem) {
+
+            if (!hit.getFace().equals(Direction.UP)) return false;
             //If world is server
             if (!worldIn.isRemote) {
                 worldIn.setBlockState(pos, origin.getDefaultState().with(BlockStateProperties.SLAB_TYPE, SlabType.BOTTOM));
@@ -54,6 +53,7 @@ public class SnowSlab extends Block {
                     worldIn.addEntity(new ItemEntity(worldIn, pos.getX() + 0.5f, pos.getY() + 0.5f, pos.getZ() + 0.5f, new ItemStack(Blocks.SNOW)));
                     player.getHeldItem(handIn).setDamage(player.getHeldItem(handIn).getDamage() - 1);
                 }
+            //If world is client
             } else {
                 worldIn.playSound(player, pos, SoundEvents.BLOCK_SNOW_BREAK, SoundCategory.BLOCKS, (SoundType.SNOW.getVolume() + 1.0F) / 2.0F, SoundType.SNOW.getPitch() * 0.8F);
             }
